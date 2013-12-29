d072cc2f43ec80c17bcd9b639bc21f3a91dad5c1
Max Berson <maxberson@gmail.com>
2013-12-29 06:24:47 -0700

    Too many changes to enumerate, really

--- js/game.js
+++ js/game.js
@@ -21,78 +21,184 @@ function Game (players) {
 //=================================== The Stack ===============================
 
 function Stack () {
-    var tiles = [ // List of all possible tiles
-                ],
-        random_seed = (function () {
-            var deck = new Array(71),
-                i;
-            for (i = 71; i--;) {
-                deck[i] = i;
-            }
-            for (i = 1000; i--;) {
-                //redacted
-                deck
-            }
-            // Randomly swap members of the array to shuffle deck
-            // Return the shuffled deck
-        }()),
-        current_card = 0;
 
-    this.draw = function() {
-        // if there are tiles left
-            // return the next one in the deck
-        // else
-            // error
-    }
+	function rand() {
+		return Math.floor(Math.random() * 24);
+	}
+
+	var available_tiles;
+
+	available_tiles = [
+		{
+			count: 2,
+			options: {
+				edges: ["f", "f", "r", "f"],
+				fields: ["m", "m", "m", "m"]
+			}
+		},
+		{
+			count: 4,
+			options: {
+				edges: ["f", "f", "f", "f"],
+				fields: ["m", "m", "m", "m"]
+			}
+		},
+		{
+			count: 1,
+			options: {
+				edges: ["c", "c", "c", "c"],
+				fields: ["c", "c", "c", "c"],
+				pennant: true
+			}
+		},
+		{
+			// The manual tells us that there are 4 of these tiles,
+			// but 1 of them is the Start tile, so we can only
+			// allow players to draw the remaining 3.
+			count: 3,
+			options: {
+				edges: ["r", "c", "r", "f"],
+				fields: [1, 2, 2, 1]
+			}
+		},
+		{
+			count: 5,
+			options: {
+				edges: ["c", "f", "f", "f"],
+				fields: [1, 1, 1, 1]
+			}
+		},
+		{
+			count: 2,
+			options: {
+				edges: ["f", "c", "f", "c"],
+				fields: ["c", "c", "c", "c"],
+				pennant: true
+			}
+		} 
+		// TODO: add tiles G through X
+	];
+
+	this.draw = function() {
+
+		var tile;
+
+		do {
+			tile = available_tiles[rand()];
+		} while (tile.count = 0);
+
+		available_tiles[i].count -= 1;
+		return (new Tile(tile.options));
+	};
 }
-
-Stack.prototype.draw = function() {
-    if (this.current_card < 72) {
-        var tile = tiles[this.random_seed[this.current_card]]
-        this.current_card++;
-        return tile
-    } else {
-        you_done_fucked_up();
-    }
-};
 
 //=========================== Tiles ===========================================
 
-// Tile containing land type data and governing placement of followers
-// Constructor should be dumb and just accept assignments from the stack
-// It doesn't need to be strict since it will only be manipulated internally
-// takes options param that is an object defining edges
-// options = {
-//   edges: [top,right,bottom,left], // (required) clockwise array of land types
-//   follower: (Follower object) // (optional) defaults to false
-//   monastery: true // (optional) defaults to false
-// }
+// A tile is divided into 8 sections:
+//
+//	 ___________
+//	|\  edge_t /|
+//	|e\_______/e|
+//	|d |  |  | d|
+//	|g |f1|f2| g|
+//	|e |--+--| e|
+//	|_ |f3|f4| _|
+//	|l |__|__| r|
+//	| / edge_b\ |
+//	|/_________\|
+
+// The edges allow us to determine which tiles fit together. They may be one of
+// field, city or road. The central 4 tiles provide additional information
+// about exactly where followers have been placed. Field sections may take the
+// following values: monastery, city, 1, 2, 3, 4. The numeric values indicate
+// which central squares are part of which field.
 
 function Tile (options) {
+	// options = { edges: [top, right, bot, left],
+	//             fields: [nw, ne, se, sw],
+	//	       pennant: ..., // boolean
+	//             follower: ... // designation for a tile section
+	//	     }
     this.edges = options.edges;
+	this.fields = options.fields;
+	this.pennant = options.pennant || false;
     this.follower = options.follower || false;
-    this.monastery = options.monastery || false;
 }
 
 Tile.prototype.rotate = function () {
-    // Shift the tile's edges array
-}
 
-// These are the edges:
-// Field only
-// City only
-// Field with road
+	var new_edges, new_fields, i, j;
 
-Tile.prototype.addFollower = function (follower) {
-    this.followers.push(follower);
-}
+	for (i = 4; i--; ) {
+		j = (i + 1) % 4;
+		new_edges[i] = this.edges[j];
+		new_fields[i] = this.fields[j];
+	}
+
+	this.edges = new_edges;
+	this.fields = new_fields;
+};
 
 //============================ The Map ========================================
 
-// Map containing tiles and governing placement rules
+// The game starts with just the Start Tile on the map. Each turn, a player
+// will add a tile to the map whose edges match with the edges of any adjacent
+// tiles that are already part of the map.
+
+// The map can also answer questions for us about features that extend across
+// multiple tiles. These features are roads, fields, cities and monasteries. We
+// will ask these questions when we update the score at the end of each turn
+// and at the very end of game.
+
 function Map () {
-    this.tiles = [];
+	var start_tile;
+
+	var check_surrounding = (function() {
+
+		var x_offsets, y_offsets;
+
+		x_offsets = [0, 1, 0, -1];
+		y_offsets = [-1, 0, 1, 0];
+		
+		return (function (coords, prop) {
+			var i, x, y, result;
+
+			result = true;
+
+			for (i = 4; i--; ) {
+				x = coords.x + x_offsets[i];
+				y = coords.y + y_offsets[i];
+				if (!prop(x, y)) {
+					result = false;
+					break;
+				}
+			}
+
+			return result;
+
+		}());
+
+	}
+
+	start_tile = new Tile ({
+		edges: ["c", "r", "f", "r"],
+		fields: [1, 1, 2, 2]
+	});
+	this.tiles[0][0] = start_tile;
+	this.get_tile = function (x, y) {
+		return this.tiles[x][y];
+	};
 }
+
+Map.prototype.add_tile = function (tile, x, y) {
+
+	// Tiles must be added to the map adjacent to at least one existing
+	// tile.
+	check_surrounding({x: x, y: y}, function (x, y) {
+		
+};
+
+//=============================== Followers ==================================
 
 // Follower of a certain type
 // types: farmer,
@@ -108,10 +214,6 @@ function is_valid_placement(new_tile, coords) {
 
     var valid_so_far = true,
 
-        // Coordinates of adjacent tiles. Top, right, bottom, left.
-        // Not the tiles themselves, but where to find them.
-        //x_offsets t = [0, 1, 0, -1], // so how does this represent adjacent tiles?
-        y_offsets = [-1, 0, 1, 0],
         i, x, y, test_edge;
 
     // Does each edge match with the tiles already on the board?
